34b021f4a6d750efa2119361150aa5d885645bd0
Kyle Bell <kyleb3218@gmail.com>
2021-09-08 19:59:00 -0400

    Python class 9.8.21

--- madlib.py
+++ madlib.py
@@ -1,17 +1,18 @@
 def printRequest(request):
+    results = []
     for x in (request):
-        input ("Give me a " + x)
-    return request
-
-
+        results.append(input("Give me a " + x)) 
+    return results
 
 #main
 request = [ "adjective:", "animal:", "vehicle:", "verb:", "color:", "noun:", "food1:", "food2:", "person:", "saying:"]
-printRequest(request)
+fillers = tuple(printRequest(request))
 
 
-
-print("Today I went to my favorite Taco Stand called the " + adjective + " " + animal + ". Unlike most food stands, they cook and prepare the food in a " + somethingRiddenIn + " while you " + verb + ". The best thing on the menu is the " + color + " " + noun + ". Instead of ground beef they fill the taco with " + food1 + ", cheese, and top it off with a salsa made from " + food2 + ". If that doesn't make your mouth water, then it's just like " + person + " always says " + saying + "!" )
+answer = "Today I went to my favorite Taco Stand called the %s %s. Unlike most food stands, they cook and prepare the food in a %s \
+while you %s. The best thing on the menu is the %s %s. Instead of ground beef they fill the taco with %s, cheese, and top it off with \
+a salsa made from %s. If that doesn't make your mouth water, then it's just like %s always says: %s!"
+print(answer % fillers)
 
 #Today I went to my favorite Taco Stand called the
 #  {adjective} {animal}. Unlike most food stands, they cook and prepare 
